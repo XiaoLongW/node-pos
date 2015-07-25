64771871a9dbcd37e41ac982d3b376eaaa40cfd4
XiaoLongW <WangXiaoLong2016@outlook.com>
2015-07-25 22:09:30 +0800

    code can run right

--- node-pos.js
+++ node-pos.js
@@ -1,18 +1,27 @@
-// #! /usr/bin/env node
-// var node-pos = require('./lib/index.js')
-//   var promotionCalculator = new PromotionCalculator();
-//   var cart = new Cart(promotionCalculator);
-//
-//   var scanner = new Scanner();
-//
-//   var pos = new Pos(scanner, cart);
-//
-//   pos.scan(tags);
-//   console.log((pos.printReceipt());
-var Item = require('./lib/item.js');
-var CartItem = require('./lib/cart-item.js');
+//var Item = require('./lib/item.js');
+//var CartItem = require('./lib/cart-item.js');
 var Pos = require('./lib/pos.js');
 var Cart = require('./lib/cart.js');
 var Scanner = require('./lib/scanner.js');
-var Promotion = require('./lib/promotion.js');
+//var Promotion = require('./lib/promotion.js');
 var PromotionCalculator = require('./lib/promotion-calculator.js');
+
+var tags = ['ITEM000001',
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000003-2',
+      'ITEM000005',
+      'ITEM000005',
+      'ITEM000005'];
+
+  var promotionCalculator = new PromotionCalculator();
+  var cart = new Cart(promotionCalculator);
+
+  var scanner = new Scanner();
+
+  var pos = new Pos(scanner, cart);
+
+  pos.scan(tags);
+  pos.printReceipt();
