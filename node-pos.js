551849835aa55ed4b35b1a9522352a1aea83463b
XiaoLongW <WangXiaoLong2016@outlook.com>
2015-07-25 16:18:18 +0800

    import item.js to node-pos.js

---
+++ node-pos.js
@@ -0,0 +1,15 @@
+// #! /usr/bin/env node
+// var node-pos = require('./lib/index.js')
+//   var promotionCalculator = new PromotionCalculator();
+//   var cart = new Cart(promotionCalculator);
+//
+//   var scanner = new Scanner();
+//
+//   var pos = new Pos(scanner, cart);
+//
+//   pos.scan(tags);
+//   console.log((pos.printReceipt());
+var Item = require('./lib/item.js');
+var item = new Item();
+item.find();
+Item.find();
