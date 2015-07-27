f4e017ff0bd3c21cf773dfab89e28a30cc0e2e66
XiaoLongW <WangXiaoLong2016@outlook.com>
2015-07-27 14:34:50 +0800

    read inputs in a file

--- node-pos.js
+++ node-pos.js
@@ -1,33 +1,22 @@
-var Pos = require('./lib/pos.js');
-var Cart = require('./lib/cart.js');
-var Scanner = require('./lib/scanner.js');
-//var Promotion = require('./lib/promotion.js');
-var PromotionCalculator = require('./lib/promotion-calculator.js');
+var Node = require('lib');
+var options = process.argv;
+var rf = require("fs");
+var inputs = rf.readFileSync(options[2],'utf-8').split("\n");
 
-var inputs = [
-  'ITEM000001',
-  'ITEM000001',
-  'ITEM000001',
-  'ITEM000001',
-  'ITEM000001',
-  'ITEM000003-2',
-  'ITEM000005',
-  'ITEM000005',
-  'ITEM000005'
-];
 function printReceipt(tags){
-  var promotionCalculator = new PromotionCalculator();
-  var cart = new Cart(promotionCalculator);
+  var promotionType = new Node.BuyTwoGetOneFree();
+  var cart = new Node.Cart(promotionType);
 
-  var scanner = new Scanner();
+  var scanner = new Node.Scanner();
 
-  var pos = new Pos(scanner, cart);
+  var pos = new Node.Pos(scanner, cart);
 
   pos.scan(tags);
+  var receipt = pos.printReceipt()
 
-  var receipt = pos.printReceipt();
   console.log(receipt);
-//  return receipt;
 }
+
 printReceipt(inputs);
+
 exports.printReceipt = printReceipt;
